d1de1849d6fa311d53a36edc4b2649df766a4d74
siem <siemarell@gmail.com>
2018-10-09 15:54:54 +0300

    multisig dialog

--- src/components/wizardDialog.tsx
+++ src/components/wizardDialog.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
-import {Dialog, FlatButton, List, ListItem, TextField} from "material-ui"
+import {Dialog, FlatButton, List, ListItem, TextField, RaisedButton, FontIcon, IconButton} from "material-ui"
+import Delete from 'material-ui/svg-icons/action/delete'
+//import {Delete, Add} from 'material-ui/svg-icons/'
 import {connect} from "react-redux"
 import {IAppState} from 'reducers'
 import {closeWizard, newEditorTab} from '../actions'
@@ -13,8 +15,8 @@ interface IWizardDialogProps {
 }
 
 class WizardDialogComponent extends React.Component<IWizardDialogProps> {
-    static ref: WizardDialogComponent
-    private publicKeys = ['a', 'b']
+    static ref: WizardDialogComponent;
+    private multisigForm = React.createRef<MultisigForm>();
 
     constructor(props: IWizardDialogProps) {
         super(props)
@@ -22,8 +24,11 @@ class WizardDialogComponent extends React.Component<IWizardDialogProps> {
     }
 
     generateContract(): string {
-        return multisig(['a', 'b', 'c'], 2)
+        if (!this.multisigForm) return;
+        const params = this.multisigForm.current.getParams();
+        return multisig(params.publicKeys, params.M)
     }
+
 
     render() {
         const buttons = {
@@ -34,45 +39,108 @@ class WizardDialogComponent extends React.Component<IWizardDialogProps> {
             label={b}
             primary={i != 0}
             onClick={() => {
-                const close = buttons[b]()
+                const close = buttons[b]();
                 if (close)
                     this.props.onClose()
             }}
         />))
 
-        return (<Dialog
-            title="Multisig contract"
+
+        return <Dialog
+            title="Multisignature contract"
             actions={actions}
+            contentStyle={ {
+                position: "absolute",
+                left: '50%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)'
+            } }
             modal={true}
+            style={{paddingTop: 0}}
+            repositionOnUpdate={true}
+            autoScrollBodyContent={true}
             open={this.props.open}
-            onRequestClose={this.props.onClose}>
-            <MultisigForm/>
-        </Dialog>)
+            onRequestClose={this.props.onClose}
+            children={
+                <MultisigForm ref={this.multisigForm}/>
+            }
+        />
+
     }
 }
 
-class MultisigForm extends React.Component<any, {publicKeys: string[], M: number}> {
-    constructor(props){
+class MultisigForm extends React.Component<any, { publicKeys: string[], M: number }> {
+    constructor(props) {
         super(props);
         this.state = {
             publicKeys: [],
             M: 0
         }
     }
-    validateBase58(base58:string){
+
+    getParams() {
+        return this.state
+    }
+
+    validateBase58(base58: string) {
         return true
+    }
+
+
+    addPublicKey = () => {
+        const currentPublicKeys = this.state.publicKeys;
+        if (currentPublicKeys.length < 8) {
+            currentPublicKeys.push('');
+        }
+        this.setState({
+            publicKeys: currentPublicKeys
+        })
+    }
+
+    updatePublicKey = (index) => (event) => {
+        const currentPublicKeys = this.state.publicKeys;
+        currentPublicKeys[index] = event.target.value;
+        this.setState({
+            publicKeys: currentPublicKeys
+        });
+    }
+
+    removePublicKey = (index) => (event) => {
+        const {publicKeys} = this.state;
+        publicKeys.splice(index, 1);
+        this.setState({publicKeys})
     }
 
     render() {
         const {publicKeys, M} = this.state;
 
-        return <React.Fragment>
-            {publicKeys.map((pk,i) =>
-                <TextField key={i}
-                           name={`PK-${i}`}
-                />
+        return <div>
+            {publicKeys.map((pk, i) =>
+                <div key={i}>
+                    <span>{`Public key ${i + 1}: `}</span>
+                    <TextField
+                        name={`PK-${i}`}
+                        value={pk}
+                        onChange={this.updatePublicKey(i)}
+                        //placeholder={`Public key ${i + 1}`}
+                        fullWidth={false}
+                        style={{width: '50%'}}
+                    />
+                    <IconButton onClick={this.removePublicKey(i)}>
+                        <Delete/>
+                    </IconButton>
+                </div>
             )}
-        </React.Fragment>
+            {this.state.publicKeys.length < 8 &&
+            <div>
+                <RaisedButton
+                    label="Add public key"
+                    onClick={this.addPublicKey}
+                    secondary={true}
+                    icon={<FontIcon className="material-icons">add</FontIcon>}
+                />
+            </div>}
+        </div>
     }
 }
 
