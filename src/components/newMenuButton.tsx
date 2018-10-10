d846a9c89eec81e325c0d1ab792e104dce027084
siem <siemarell@gmail.com>
2018-10-10 17:56:04 +0300

    halfway newmenubutton

--- src/components/newMenuButton.tsx
+++ src/components/newMenuButton.tsx
@@ -1,5 +1,8 @@
 import * as React from "react"
-import {MenuItem, Popover, Menu, FlatButton, FontIcon} from 'material-ui'
+import {Popover, FontIcon} from 'material-ui'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import {connect} from 'react-redux'
 import {loadSample, newEditorTab, openWizard} from '../actions'
 import {palette} from '../style'
@@ -9,81 +12,87 @@ class NewMenuButtonComponent extends React.Component
         onLoadSample: (key: string) => void
         onNewContract: (code: string) => void
         onWizard: (kind: string) => void
-    }, { isMenuOpen: boolean, anchorEl: any }> {
+    }, { anchorEl: any }> {
 
 
     constructor(props) {
         super(props);
         this.state = {
-            isMenuOpen: false,
             anchorEl: null
         }
     }
 
-    handleClick(event: React.MouseEvent<{}>) {
-        event.preventDefault();
-        this.setState({isMenuOpen: true, anchorEl: event.currentTarget})
+    handleClick = (event: React.MouseEvent<{}>) => {
+        //event.preventDefault();
+        this.setState({anchorEl: event.currentTarget})
     }
 
-    handleMenuClose() {
-        this.setState({isMenuOpen: false})
+    handleClose = () => {
+        this.setState({anchorEl: null});
     }
 
-    handleLoadSample(key: string) {
-        this.handleMenuClose()
+    handleLoadSample = (key: string) => {
+        this.handleClose()
         this.props.onLoadSample(key)
     }
 
-    handleWizard(kind: string) {
-        this.handleMenuClose()
+    handleWizard = (kind: string) => {
+        this.handleClose()
         this.props.onWizard(kind)
     }
 
-    clear() {
-        this.handleMenuClose()
+    clear = () => {
+        this.handleClose()
         this.props.onNewContract('')
     }
 
     render() {
-        return (
-            <span>
-        <FlatButton
-            onClick={(e) => this.handleClick(e)}
-            icon={<FontIcon className="material-icons">add</FontIcon>}
-            label='New'
-            backgroundColor={palette.accent1Color}
-            hoverColor='#ffb3cb'
-            style={{color: 'white', backgroundColor: '#1f5af6', marginLeft: 30, paddingBottom: 37}}
+        const {anchorEl} = this.state
+        return (<span>
+        <Button
+            variant="text"
+            aria-owns={anchorEl ? 'new-menu' : null}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+            children={[<FontIcon className="material-icons">add</FontIcon>, 'New']}
+            //label='New'
+            //backgroundColor={palette.accent1Color}
+            //hoverColor='#ffb3cb'
+            //style={{color: 'white', backgroundColor: '#1f5af6', marginLeft: 30, paddingBottom: 37}}
         />
-        <Popover
-            open={this.state.isMenuOpen}
-            anchorEl={this.state.anchorEl}
-            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}
-            onRequestClose={(e) => this.handleMenuClose()}>
-          <Menu>
-            <MenuItem primaryText="Empty contract"
-                      leftIcon={<FontIcon className="material-icons">insert_drive_file</FontIcon>}
-                      onClick={() => this.clear()}
-            />
-            <MenuItem primaryText="Sample"
-                      leftIcon={<FontIcon className="material-icons">remove_red_eye</FontIcon>}
-                      rightIcon={<FontIcon className="material-icons">arrow_right</FontIcon>}
-                      menuItems={[
-                          <MenuItem primaryText="Simple" onClick={() => this.handleLoadSample('simple')}/>,
-                          <MenuItem primaryText="Multisig (2 of 3)" onClick={() => this.handleLoadSample('multisig')}/>,
-                          <MenuItem primaryText="Notary" onClick={() => this.handleLoadSample('notary')}/>,
-                      ]}
-            />
-            <MenuItem primaryText="Wizard"
-                      leftIcon={<FontIcon className="material-icons">flash_on</FontIcon>}
-                      rightIcon={<FontIcon className="material-icons">arrow_right</FontIcon>}
-                      menuItems={[
-                          <MenuItem primaryText="Multisig" onClick={() => this.handleWizard('multisig')}/>
-                      ]}
-            />
+          <Menu id="new-menu"
+                open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                onClose={this.handleClose}
+          >
+            <MenuItem onClick={() => this.clear()}>
+                <FontIcon className="material-icons">insert_drive_file</FontIcon>
+                Empty contract
+            </MenuItem>
+              <MenuItem
+                  // menuItems={[
+                  // <MenuItem children="Simple" onClick={() => this.handleLoadSample('simple')}/>,
+                  // <MenuItem children="Multisig (2 of 3)" onClick={() => this.handleLoadSample('multisig')}/>,
+                  // <MenuItem children="Notary" onClick={() => this.handleLoadSample('notary')}/>,
+                  // ]}
+              >
+                  <FontIcon className="material-icons">remove_red_eye</FontIcon>
+                  Sample
+                  <FontIcon className="material-icons">arrow_right</FontIcon>
+              </MenuItem>
+              <MenuItem>
+                  <FontIcon className="material-icons">flash_on</FontIcon>
+                  Wizard
+                  <FontIcon className="material-icons">arrow_right</FontIcon>
+              </MenuItem>
+              {/*<MenuItem primaryText="Wizard"*/}
+              {/*leftIcon={<FontIcon className="material-icons">flash_on</FontIcon>}*/}
+              {/*rightIcon={<FontIcon className="material-icons">arrow_right</FontIcon>}*/}
+              {/*menuItems={[*/}
+              {/*<MenuItem primaryText="Multisig" onClick={() => this.handleWizard('multisig')}/>*/}
+              {/*]}*/}
+              />
           </Menu>
-        </Popover>
       </span>
         )
     }
