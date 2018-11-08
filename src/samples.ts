c0b63dbf79be892a5b54ad1ab763995aa1312661
siem <siemarell@gmail.com>
2018-11-08 16:01:18 +0300

    sample pk

--- src/samples.ts
+++ src/samples.ts
@@ -4,7 +4,7 @@ export const codeSamples: {
   [id in sampleTypes]: string
 } = {
   simple: `
-let publicKey = base58'7kPFrHDiGw1rCm7LPszuECwWYL3dMf6iMifLRDJQZMzy'
+let publicKey = base58'EmvRZzBZRgCwoW8b88TsAr9KYeutTKEogSwiYbFxfoff'
 let heightBefore = 100
 let AC = sigVerify(tx.bodyBytes, tx.proofs[0], publicKey)
 let heightVerification = height > heightBefore + 10
