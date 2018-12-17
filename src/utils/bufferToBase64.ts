8246bec7a7c9e47fdfd8ffbe43201a28d8091409
siem <siemarell@gmail.com>
2018-12-17 10:57:37 +0300

    ride-js update

--- src/utils/bufferToBase64.ts
+++
@@ -1,6 +0,0 @@
-export function bufferToBase64(buf: any) {
-  var binstr = Array.prototype.map.call(buf, function (ch:any) {
-    return String.fromCharCode(ch);
-  }).join('')
-  return btoa(binstr)
-}
--- src/utils/safeCompile.ts
+++ src/utils/safeCompile.ts
@@ -1,5 +1,6 @@
 import {compile} from '@waves/ride-js'
-import {bufferToBase64} from "./bufferToBase64";
+import base64 from 'base64-js'
+
 
 export interface ICompilationResult {
     result?: string
@@ -10,8 +11,8 @@ export interface ICompilationResult {
 export function safeCompile(code: string): ICompilationResult {
     let result: any
     try {
-        result = compile(code)
-        if (result.result) result.result = bufferToBase64(new Uint8Array(result.result))
+        result = compile(code);
+        if (result.result) result.result = base64.fromByteArray(Uint8Array.from(result.result))
     }catch (e) {
         if (typeof e === 'object'){
             result = {
