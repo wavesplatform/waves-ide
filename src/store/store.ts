7136cdd8a20f68191c833f2eb6475936f10e624c
anagorny <anagorny@wv00124.local>
2019-02-20 13:10:32 +0300

    load_state_from_JSON

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,22 @@
-import {createStore, applyMiddleware, compose, Middleware} from 'redux';
+import { createStore, applyMiddleware, compose, Middleware } from 'redux';
 import rootReducer from './root-reducer';
-import {syncEnvMW} from "./repl-sync";
-import {loadState} from "../utils/localStore";
-import {createLogger} from "redux-logger";
-import {fileManagerMW} from "./file-manager-mw";
+import { syncEnvMW } from './repl-sync';
+import { loadState } from '../utils/localStore';
+import { createLogger } from 'redux-logger';
+import { fileManagerMW } from './file-manager-mw';
+import crypto from 'crypto';
+const Storage = localStorage;
 
 const composeEnhancers =
     (window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 function configureStore(initialState?: object) {
     // configure middlewares
-    const middlewares: Middleware[] = [syncEnvMW, fileManagerMW];
+    const middlewares: Middleware[] = [ syncEnvMW, fileManagerMW ];
 
     if (process.env.NODE_ENV !== 'production') {
-        const loggerMiddleware = createLogger()
-        middlewares.push(loggerMiddleware)
+        const loggerMiddleware = createLogger();
+        middlewares.push(loggerMiddleware);
     }
     // compose enhancers
     const enhancer = composeEnhancers(applyMiddleware(...middlewares));
@@ -23,7 +25,27 @@ function configureStore(initialState?: object) {
 }
 
 // pass an optional param to rehydrate state on app start
+
+if ( process.env.IS_LOAD_FROM_JSON ){
+    getStateFromJson();
+}
+
 const loadedState = loadState();
 const store = configureStore(loadedState);
 // export store singleton instance
+
 export default store;
+
+function getStateFromJson() {
+    let store;
+    try{
+        store = JSON.stringify(require('../assets/jsonStore.json'));
+        let hash = crypto.createHash('md5').update(store).digest('hex');
+        if ( hash !== localStorage.getItem('hash') ){
+            localStorage.setItem( 'store', store);
+            Storage.setItem('hash', hash);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
