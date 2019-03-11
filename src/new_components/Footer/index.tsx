677a3f1484f6f624d29f9786e555cfbfd21f170d
anagorny <anagorny@wv00124.local>
2019-03-11 13:46:44 +0300

    add footer component

---
+++ src/new_components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+    <footer>
+        <div className="left">
+            <a target="_blank" href="/">Demotour</a>
+            <a target="_blank" href="/">Hotkeys</a>
+            <a target="_blank" href="https://docs.wavesplatform.com/en/smart-contracts/waves-contracts-language-description.html">Docs</a>
+            <a target="_blank" href="/">Community</a>
+        </div>
+        <div className="right">
+            <a target="_blank" href="https://github.com/wavesplatform/waves-ide">Waves IDE on GitHub</a>
+        </div>
+    </footer>
+);
