bd43f2eaf2dbe49eba1e1599ff12a2aa543c9906
Alexey Nagorny <anagorny@wavesplatform.com>
2019-03-17 15:51:31 +0300

    add footer

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Editor from '@components/Editor';
 import TopBar from '@components/TopBar';
 import EditorTabs from '@components/EditorTabs';
+import Footer from './new_components/editorsFooter';
 import { Intro } from '@components/intro';
 import { UserNotification } from '@components/UserNotification';
 import { UserDialog } from '@components/UserDialog';
@@ -165,8 +166,9 @@ export class AppComponent extends React.Component<IAppProps> {
                                 :
                                 <Intro/>
                             }
+                            <Footer/>
                         </div>
-                        <RightTabs className={classes!.rightTabsField}/>
+                        {/*<RightTabs className={classes!.rightTabsField}/>*/}
                     </div>
 
                     <ReplWrapper theme="light" name="testRepl"/>
