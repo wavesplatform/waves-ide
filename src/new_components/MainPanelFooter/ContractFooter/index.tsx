a954000dbb1653d149f8981e3d79401727b98c78
Alexey Nagorny <anagorny@wavesplatform.com>
2019-03-25 16:27:01 +0300

    replase contract footer logic

--- src/new_components/MainPanelFooter/ContractFooter/index.tsx
+++ src/new_components/MainPanelFooter/ContractFooter/index.tsx
@@ -1,28 +1,116 @@
 import React from 'react';
 import ScriptComplexity from './ScriptComplexity';
 import styles from '../styles.less';
+import { FILE_TYPE, FilesStore, IFile, SettingsStore, SignerStore } from '@stores';
+import { issue, setAssetScript, setScript } from '@waves/waves-transactions';
+import * as RideJS from '@waves/ride-js';
+import { inject, observer } from 'mobx-react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { copyToClipboard } from '@utils/copyToClipboard';
+import notification from 'rc-notification';
+
+type TNotification = { notice: (arg0: { content: string; }) => void; };
+
 
 interface IContractFooterProps {
-    scriptSize?: number
-    nodeUrl?: string
-    base64?: string
-    copyBase64Handler?: () => void
-    issueHandler?: () => void
-    deployHandler?: () => void
-    className: string
+    className: string,
+    file: IFile,
 }
 
-class ContractFooter extends React.Component<IContractFooterProps> {
+interface IInjectedProps {
+    filesStore?: FilesStore
+    settingsStore?: SettingsStore
+    signerStore?: SignerStore
+}
+
+interface IProps extends IInjectedProps, RouteComponentProps, IContractFooterProps {
+}
+
+
+@inject('filesStore', 'settingsStore', 'signerStore')
+@observer
+class ContractFooter extends React.Component<IProps> {
+
+    handleDeploy = (base64: string) => {
+        const {history, settingsStore, signerStore} = this.props;
+        const chainId = settingsStore!.defaultNode!.chainId;
+        const file = this.props.file;
+        let tx;
+        if (file.type === FILE_TYPE.ACCOUNT_SCRIPT) {
+            tx = setScript({
+                script: base64,
+                chainId: chainId,
+                senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr' // Dummy senderPk Only to create tx
+            });
+            delete tx.senderPublicKey;
+            delete tx.id;
+        }
+        if (file.type === FILE_TYPE.ASSET_SCRIPT) {
+            tx = setAssetScript({
+                assetId: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr', //Dummy assetId
+                script: base64,
+                chainId: chainId,
+                senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr', // Dummy senderPk Only to create tx
+            });
+            delete tx.senderPublicKey;
+            delete tx.assetId;
+            delete tx.id;
+        }
+
+        if (tx != null) {
+            signerStore!.setTxJson(JSON.stringify(tx, null, 2));
+            history.push('signer');
+        }
+    };
+
+    handleIssue = (base64: string) => {
+        const {history, settingsStore, signerStore} = this.props;
+        const chainId = settingsStore!.defaultNode!.chainId;
+
+        const tx = issue({
+            script: 'base64:' + base64,
+            name: 'test',
+            description: 'test',
+            quantity: 1000,
+            reissuable: true,
+            chainId: chainId,
+            senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr' // Dummy senderPk Only to create tx
+        });
+        delete tx.senderPublicKey;
+        delete tx.id;
+        delete tx.description;
+        delete tx.name;
+        delete tx.quantity;
+
+        signerStore!.setTxJson(JSON.stringify(tx, null, 2));
+        history.push('signer');
+    };
+
+    handleCopyBase64 = (base64: string) => {
+        if (copyToClipboard(base64)) {
+            notification.newInstance({}, (notification: TNotification) => {
+                notification.notice({content: 'Copied!'});
+            });
+        }
+    };
+
     render() {
-        const {
-            scriptSize,
-            nodeUrl,
-            base64,
-            copyBase64Handler,
-            deployHandler,
-            issueHandler,
-            className
-        } = this.props;
+        const {className, file, filesStore} = this.props;
+        let nodeUrl, base64: any, scriptSize, copyBase64Handler, issueHandler, deployHandler;
+
+        if (file.content) {
+            const compilationResult = RideJS.compile(file.content);
+            if (!('error' in compilationResult)) {
+                scriptSize = compilationResult.result.size;
+                base64 = compilationResult.result.base64;
+                // Todo: default node!!
+                nodeUrl = filesStore!.rootStore.settingsStore.defaultNode!.url;
+                copyBase64Handler = base64 ? () => this.handleCopyBase64(base64) : undefined;
+                issueHandler =
+                    base64 && file.type === FILE_TYPE.ASSET_SCRIPT ? () => this.handleIssue(base64) : undefined;
+                deployHandler = base64 ? () => this.handleDeploy(base64) : undefined;
+            }
+        }
 
         return <footer className={className}>
             <ScriptComplexity nodeUrl={nodeUrl} base64={base64} scriptSize={scriptSize}/>
@@ -42,4 +130,4 @@ class ContractFooter extends React.Component<IContractFooterProps> {
     }
 }
 
-export default ContractFooter;
+export default (withRouter(ContractFooter));
--- src/new_components/MainPanelFooter/index.tsx
+++ src/new_components/MainPanelFooter/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { RouteComponentProps, withRouter } from 'react-router';
-import * as RideJS from '@waves/ride-js';
-import { issue, setAssetScript, setScript } from '@waves/waves-transactions';
 import 'rc-notification/assets/index.css';
-import notification from 'rc-notification';
-import { FilesStore, FILE_TYPE, IFile, SettingsStore, SignerStore, NotificationsStore } from '@stores';
-import { copyToClipboard } from '@utils/copyToClipboard';
+import { FilesStore, FILE_TYPE } from '@stores';
 import ContractFooter from './ContractFooter';
 import EmptyFooter from './EmptyFooter';
 import TestFooter from './TestFooter';
@@ -14,117 +9,24 @@ import styles from './styles.less';
 
 interface IInjectedProps {
     filesStore?: FilesStore
-    settingsStore?: SettingsStore
-    signerStore?: SignerStore
-    notificationsStore?: NotificationsStore
 }
 
-type TNotification = { notice: (arg0: { content: string; }) => void; }
-
-interface IFooterProps extends IInjectedProps, RouteComponentProps {
-}
-
-@inject('filesStore', 'settingsStore', 'signerStore', 'notificationsStore')
+@inject('filesStore')
 @observer
-class MainPanelFooter extends React.Component <IFooterProps> {
-
-    handleDeploy = (base64: string, file: IFile) => {
-        const {history, settingsStore, signerStore} = this.props;
-        const chainId = settingsStore!.defaultNode!.chainId;
-
-        let tx;
-        if (file.type === FILE_TYPE.ACCOUNT_SCRIPT) {
-            tx = setScript({
-                script: base64,
-                chainId: chainId,
-                senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr' // Dummy senderPk Only to create tx
-            });
-            delete tx.senderPublicKey;
-            delete tx.id;
-        }
-        if (file.type === FILE_TYPE.ASSET_SCRIPT) {
-            tx = setAssetScript({
-                assetId: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr', //Dummy assetId
-                script: base64,
-                chainId: chainId,
-                senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr', // Dummy senderPk Only to create tx
-            });
-            delete tx.senderPublicKey;
-            delete tx.assetId;
-            delete tx.id;
-        }
-
-        if (tx != null) {
-            signerStore!.setTxJson(JSON.stringify(tx, null, 2));
-            history.push('signer');
-        }
-    };
-
-    handleIssue = (base64: string) => {
-        const {history, settingsStore, signerStore} = this.props;
-        const chainId = settingsStore!.defaultNode!.chainId;
-
-        const tx = issue({
-            script: 'base64:' + base64,
-            name: 'test',
-            description: 'test',
-            quantity: 1000,
-            reissuable: true,
-            chainId: chainId,
-            senderPublicKey: 'DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr' // Dummy senderPk Only to create tx
-        });
-        delete tx.senderPublicKey;
-        delete tx.id;
-        delete tx.description;
-        delete tx.name;
-        delete tx.quantity;
-
-        signerStore!.setTxJson(JSON.stringify(tx, null, 2));
-        history.push('signer');
-    };
-
-    getContractFooter = (file: IFile, nodeUrl: string) => {
-        const compilationResult = RideJS.compile(file.content);
-        if ('error' in compilationResult) {
-            return <ContractFooter className={styles!.root}/>;
-        }
-        const base64 = compilationResult.result.base64;
-        // Todo: default node!!
-        return <ContractFooter
-            className={styles!.root}
-            scriptSize={compilationResult.result.size}
-            base64={base64}
-            nodeUrl={nodeUrl}
-            copyBase64Handler={() => {
-                if (copyToClipboard(base64)) {
-                    notification.newInstance({}, (notification: TNotification) => {
-                        notification.notice({content: 'Copied!'});
-                    });
-                }
-            }}
-            issueHandler={file.type === FILE_TYPE.ASSET_SCRIPT ? (() => this.handleIssue(base64)) : undefined}
-            deployHandler={() => this.handleDeploy(base64, file)}
-        />;
-    };
-
+class MainPanelFooter extends React.Component <IInjectedProps> {
     render() {
         const {filesStore} = this.props;
         const file = filesStore!.currentFile;
         let footer;
-        if (!file || !file.content) {
+        if (!file) {
             footer = <EmptyFooter className={styles!.root}/>;
-        } else {
-            switch (true) {
-                case (file.type === FILE_TYPE.TEST):
-                    footer = <TestFooter className={styles!.root}/>;
-                    break;
-                case ((file.type === FILE_TYPE.ASSET_SCRIPT || file.type === FILE_TYPE.ACCOUNT_SCRIPT)):
-                    footer = this.getContractFooter(file, filesStore!.rootStore.settingsStore.defaultNode!.url);
-                    break;
-            }
+        } else if (file.type === FILE_TYPE.TEST) {
+            footer = <TestFooter className={styles!.root}/>;
+        } else if ((file.type === FILE_TYPE.ASSET_SCRIPT || file.type === FILE_TYPE.ACCOUNT_SCRIPT)) {
+            footer = <ContractFooter className={styles!.root} file={file}/>;
         }
         return footer;
     }
 }
 
-export default (withRouter(MainPanelFooter));
+export default (MainPanelFooter);
