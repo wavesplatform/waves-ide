3eeaf6f3268f5c88b9a050d000f3c123a7e8b82e
siem <siemarell@gmail.com>
2019-03-27 10:24:37 +0300

    added global styles and icons

--- src/new_components/Tabs/Tab.tsx
+++ src/new_components/Tabs/Tab.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import classname from 'classnames';
 import styles from './styles.less';
+import { TTabInfo } from '@stores/TabsStore';
 
 export interface ITabProps {
-    label: string
+    info: TTabInfo
     active: boolean
     onClick?: () => void
     onClose?: () => void
 }
 
+const typeIconClassMap: Record<string, {active: string, inactive: string}> = {
+    'welcome': { active: 'accountdoc-16-submit-400', inactive: 'systemdoc-16-basic-600' },
+    'assetScript': { active: 'accountdoc-16-submit-400', inactive: 'assetdoc-16-basic-600' },
+    'accountScript': { active: 'accountdoc-16-submit-400', inactive: 'accountdoc-16-basic-600' },
+    'test': { active: 'accountdoc-16-submit-400', inactive: 'accountdoc-16-basic-600' }
+};
+
 export default class Tab extends React.Component<ITabProps> {
     render() {
         let className = styles.tab;
+        const active = this.props.active ? 'active' : 'inactive';
         if (this.props.active) className = classname(className, styles['active-tab']);
 
-        const {onClick, onClose, label} = this.props;
+        const {onClick, onClose, info} = this.props;
         return <div className={className}>
-            <div style={{border: '1px solid red', minWidth: 16, height: 16}}/>
-            <span className={styles['tab-text']} onClick={onClick}>{label}</span>
+            <div className={typeIconClassMap[info.type] && typeIconClassMap[info.type][active]}/>
+            <span className={styles['tab-text']} onClick={onClick}>{info.label}</span>
             <button style={{width: 22}} onClick={onClose}>x</button>
         </div>;
     }
