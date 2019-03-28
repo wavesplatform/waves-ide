b00eb58213dbb263d62f925db29c5251e2daf45b
Alexey Nagorny <anagorny@wavesplatform.com>
2019-03-28 11:13:59 +0300

    in process

--- src/new_components/TopBar/index.tsx
+++ src/new_components/TopBar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './styles.less';
-import icons from '../icons';
+import notification from 'rc-notification';
 import TestReplMediatorContext from '@utils/ComponentsMediatorContext';
+
+type TNotification = { notice: (arg0: { content: string; }) => void; };
 
 
 export default class TopBar extends React.Component {
@@ -10,9 +12,14 @@ export default class TopBar extends React.Component {
     context!: React.ContextType<typeof TestReplMediatorContext>;
 
     isDarkTheme = false;
+    fontSize = 12;
 
-    changeSize(event: any) {
-        this.context!.dispatch('actions.fontSize', event.target.value || 12);
+    changeSize() {
+        this.fontSize = this.fontSize >= 20 ? 8 : this.fontSize + 2;
+        this.context!.dispatch('actions.fontSize', this.fontSize);
+        notification.newInstance({}, (notification: TNotification) => {
+            notification.notice({content: `Font size is ${this.fontSize} px`});
+        });
     }
 
     changeTheme() {
@@ -24,23 +31,9 @@ export default class TopBar extends React.Component {
 
     render() {
         return (<div className={styles.root}>
-            <div className={styles.iconBtn} onClick={() => this.context!.dispatch('actions.find')}>
-                {icons.search}
-            </div>
-            <div className={styles.iconBtn}>
-                <select defaultValue={'12'} ref={'fontSize'} onChange={this.changeSize.bind(this)}>
-                    <option value="8">8px</option>
-                    <option value="10">10px</option>
-                    <option value="12" >12px</option>
-                    <option value="14">14px</option>
-                    <option value="16">16px</option>
-                    <option value="18">18px</option>
-                    <option value="20">20px</option>
-                </select>
-            </div>
-            <div className={styles.iconBtn} onClick={() => this.changeTheme()}>
-                {icons.bulb}
-            </div>
+            <div className="search-16-basic-500" onClick={() => this.context!.dispatch('actions.find')}/>
+            <div className="font-16-basic-500" onClick={() => this.changeSize()}/>
+            <div className="darktheme-16-basic-500" onClick={() => this.changeTheme()}/>
         </div>);
     }
 }
