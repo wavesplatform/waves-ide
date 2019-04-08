1f6b8bd2397375754e03e18d7fc593957327e896
chlenc <35231749+chlenc@users.noreply.github.com>
2019-04-08 18:44:40 +0300

    Ide172/pop ups (#84)
    * add dialog component

---
+++ src/new_components/Dialog/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Dialog from 'rc-dialog';
+import styles from './styles.less';
+import classNames = require('classnames');
+
+interface IDialogProps {
+    title?: string
+    footer?: React.ReactNode
+    onClose?: () => void
+    className?: string
+    width?: number
+    height?: number
+    children?: any
+    visible?: boolean
+}
+
+export default (props: IDialogProps) =>
+    <Dialog
+        title={props.title}
+        footer={props.footer}
+        onClose={props.onClose}
+        className={classNames(styles.root, props.className)}
+        width={props.width}
+        height={props.height}
+        visible={props.visible || true}
+    >
+        {props.children}
+    </Dialog>;
