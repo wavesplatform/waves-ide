655226dc2d84d4638bb581ba73652d5f5a901b65
Alexey Nagorny <anagorny@wavesplatform.com>
2019-04-08 19:37:11 +0300

    add second button

--- src/new_components/WorkPanel/index.tsx
+++ src/new_components/WorkPanel/index.tsx
@@ -10,7 +10,8 @@ import TabContent from '@src/new_components/TabContent';
 import SidePanel from '@src/new_components/SidePanel';
 import MainPanelFooter from '@src/new_components/MainPanelFooter';
 import NewFileBtn from '@src/new_components/NewFileBtn';
-import SettingsBtn from '@src/new_components/SettingsBtn/alternative';
+import SettingsBtn from '@src/new_components/SettingsBtn';
+import SettingsBtnAlternative from '@src/new_components/SettingsBtn/alternative';
 import EditorTopBar from '@src/new_components/EditorTopBar';
 
 import styles from './styles.less';
@@ -39,6 +40,7 @@ export default class WorkPanel extends React.Component<IInjectedProps> {
                             account
                         </div>
                         <SettingsBtn/>
+                        <SettingsBtnAlternative/>
                     </div>
                     <div className={styles.mainPanel_content}>
                         <EditorTopBar/>
