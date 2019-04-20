9e1020ce7f77c80e62f52101bbd9d83838560774
Alexey Nagorny <anagorny@wavesplatform.com>
2019-04-20 14:38:42 +0300

    add accounts hide on outside click

--- src/new_components/Accounts/index.tsx
+++ src/new_components/Accounts/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import classNames from 'classnames';
 import { inject, observer } from 'mobx-react';
 
 import { AccountsStore } from '@stores';
-
-import Scrollbar from '@src/new_components/Scrollbar';
-
-import Avatar from '@src/new_components/Avatar';
-import AccountItem from '@src/new_components/Accounts/AccountItem';
-import AccountInfo from '@src/new_components/Accounts/AccountInfo';
+import classNames from 'classnames';
 
 import styles from './styles.less';
-
+import Avatar from '@src/new_components/Avatar';
+import AccountInfo from '@src/new_components/Accounts/AccountInfo';
+import AccountItem from '@src/new_components/Accounts/AccountItem';
+import Scrollbar from '@src/new_components/Scrollbar';
 
 interface IInjectedProps {
     accountsStore?: AccountsStore
@@ -28,37 +25,66 @@ interface IAccountState {
 @inject('accountsStore')
 @observer
 export default class Accounts extends React.Component<IAccountProps, IAccountState> {
-    state = {isOpen: false};
+    constructor(props: IAccountProps) {
+        super(props);
 
-    private generateAccount = () => this.props.accountsStore!.generateAccount();
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
 
-    private changeOpenStatus = () => this.setState({isOpen: !this.state.isOpen});
-    
+    wrapperRef: any;
+
+    changeOpenStatus = () => this.setState({isOpen: !this.state.isOpen});
+
+    setHiddenStatus = () => this.setState({isOpen: false});
+
+    generateAccount = () => this.props.accountsStore!.generateAccount();
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+
+    setWrapperRef(node: any) {
+        this.wrapperRef = node;
+    }
+
+    handleClickOutside(event: Event) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            this.setHiddenStatus();
+        }
+    }
+
     render() {
         const {isOpen} = this.state;
         const {className, accountsStore} = this.props;
         const activeAccount = accountsStore!.activeAccount;
         const activeAccountIndex = accountsStore!.activeAccountIndex;
-
-        return <div className={classNames(styles.root, className)}>
+        return <div ref={this.setWrapperRef} className={classNames(styles.root, className)}>
             <div className={styles.head} onClick={this.changeOpenStatus}>
-                {activeAccount ?
-                    (<div className={styles.head_info}>
-                            <Avatar size={32} className={styles.head_avatar} address={activeAccount.privateKey}/>
-                            <div className={styles.head_textContainer}>
-                                <div className={styles.head_name}>{activeAccount!.label}</div>
-                                <div className={styles.head_status}>
-                                    <div className={styles.head_indicator}/>
-                                    Active
-                                </div>
+                                {activeAccount ?
+                (<div className={styles.head_info}>
+                        <Avatar size={32} className={styles.head_avatar} address={activeAccount.privateKey}/>
+                        <div className={styles.head_textContainer}>
+                            <div className={styles.head_name}>{activeAccount!.label}</div>
+                            <div className={styles.head_status}>
+                                <div className={styles.head_indicator}/>
+                                Active
                             </div>
                         </div>
-                    ) : (
-                        <div className={styles.head_info}>
-                            <div className={styles.head_login}/>
-                            <div className={styles.head_name}>Add Account</div>
-                        </div>)
-                }
+                    </div>
+                ) : (
+                    <div className={styles.head_info}>
+                        <div className={styles.head_login}/>
+                        <div className={styles.head_name}>Add Account</div>
+                    </div>)
+            }
                 <div className={classNames(styles.head_arrow, {[styles.head_arrow__open]: isOpen})}/>
             </div>
             {isOpen && (
@@ -92,3 +118,4 @@ export default class Accounts extends React.Component<IAccountProps, IAccountSta
         </div>;
     }
 }
+
