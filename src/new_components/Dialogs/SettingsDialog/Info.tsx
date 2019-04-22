ee64b321147b4752e01b9ae2a8f7766380238ea8
Alexey Nagorny <anagorny@wavesplatform.com>
2019-04-22 16:47:05 +0300

    IDE-241

--- src/new_components/Dialogs/SettingsDialog/Info.tsx
+++ src/new_components/Dialogs/SettingsDialog/Info.tsx
@@ -2,19 +2,51 @@ import * as React from 'react';
 import styles from '@src/new_components/Dialogs/SettingsDialog/styles.less';
 import Tooltip from '@src/new_components/Tooltip';
 
-const Info = () =>
+interface IProps {
+    infoType?: 'Mainnet' | 'Testnet'
+}
+
+type TDataItem = {
+    title: string,
+    text: string,
+    more: string
+};
+
+type TInfoData = {
+    Mainnet: TDataItem,
+    Testnet: TDataItem
+};
+
+const infoData: TInfoData = {
+    Mainnet: {
+        title: 'Mainnet',
+        text: 'Mainnet – short for main network – is the original and functional blockchain where actual transactions' +
+            ' take place in the distributed ledger and the native cryptocurrencies possess real economic value.',
+        more: 'https://docs.wavesplatform.com/en/waves-environment/waves-mainnet.html#section-96c5b680517adda6186330' +
+            'd3d3ab9737'
+    },
+    Testnet: {
+        title: 'Testnet',
+        text: 'The testnet(test network) is an alternative Waves blockchain, to be used for testing.',
+        more: 'https://docs.wavesplatform.com/en/waves-environment/waves-mainnet.html#section-96c5b680517adda6186330' +
+            'd3d3ab9737'
+    }
+};
+
+const Info = ({infoType}: IProps) =>
     <Tooltip placement="bottomLeft" trigger="hover" align={{offset: [-34, 0]}}
              overlay={
-                 <div>
-                     <div className={styles.tooltip_title}>Headline</div>
-                     <div className={styles.tooltip_text}>Once the transaction is confirmed, the gateway will
-                         process the transfer of BTC to a token in your Waves account.
+                 !infoType
+                     ? <div/>
+                     : <div>
+                         <div className={styles.tooltip_title}>{infoData[infoType].title}</div>
+                         <div className={styles.tooltip_text}>{infoData[infoType].text}</div>
+                         <a className={styles.tooltip_more} href={infoData[infoType].more} target="_blank">Show more</a>
                      </div>
-                     <div className={styles.tooltip_more}>Show more</div>
-                 </div>
              }
     >
         <div className={styles.info}/>
     </Tooltip>;
+
 
 export default Info;
