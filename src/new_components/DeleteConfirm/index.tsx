c39f7f3e47035c7ddd418a8169d498d78d51e8fe
Алешенька:3 <alexnagorny.an@gmail.com>
2019-04-25 00:07:52 +0300

    design

--- src/new_components/DeleteConfirm/index.tsx
+++ src/new_components/DeleteConfirm/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tooltip from '@src/new_components/Tooltip';
 import Button from '@src/new_components/Button';
-import styles from './styles.less'
+import styles from './styles.less';
 
 interface IProps {
     children: JSX.Element
@@ -13,20 +13,19 @@ export default class DeleteConfirm extends React.Component<IProps> {
 
     overlay = <div className={styles.root}>
         <div className={styles.bold}>Delete this account?</div>
-        <div>
-            <div>Are you sure you want to</div>
-            <div className={styles.text}>permenantly delete</div>
+        <div>Are you sure you want to</div>
+        <div className={styles.text}>{'permenantly delete '}
             <div className={styles.name}>{this.props.name}</div>
         </div>
-        <div>
-            <Button>Cancel</Button>
-            <Button type="action-blue" onClick={this.props.onDelete}>Add</Button>
+        <div className={styles.buttonSet}>
+            <Button className={styles.btn}>Cancel</Button>
+            <Button className={styles.btn} type="action-blue" onClick={this.props.onDelete}>Yes, delete it</Button>
         </div>
     </div>;
 
     render() {
         const {children} = this.props;
-        return <Tooltip placement="bottomLeft" align={{offset: [-34, 0]}} overlay={this.overlay} trigger="click">
+        return <Tooltip placement="bottomLeft"  overlay={this.overlay} trigger="click">
             {children}
         </Tooltip>;
     }
