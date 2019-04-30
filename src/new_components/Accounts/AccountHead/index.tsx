010836bff104956299460af1bde7d036e56c1ec7
Sergey B <siemarell@users.noreply.github.com>
2019-04-30 17:50:41 +0300

    Ide 269/accounts refactor (#165)
    * dialog import refactored out
    
    * accounthead refactored out
    
    * account ifnfo refactored out
    
    * removed notification duplication
    
    * account item refactored out

---
+++ src/new_components/Accounts/AccountHead/index.tsx
@@ -0,0 +1,47 @@
+import { observer } from 'mobx-react';
+import { IAccount } from '@stores';
+import Avatar from '@src/new_components/Avatar';
+import React from 'react';
+import styles from './styles.less';
+import classNames from 'classnames';
+
+interface IProps {
+    account?: IAccount
+    onClick: () => void
+    isOpened: boolean
+}
+
+const visibleBalance = (balance?: number) => Math.round((balance || 0) / 10 ** 6) / 10 ** 2;
+
+const AccountHead = observer(({account, onClick, isOpened}: IProps) => (
+    <div className={styles.root} onClick={onClick}>
+        {account == null
+            ?
+            <NoAccount/>
+            :
+            <div className={styles.content}>
+                <Avatar size={32} className={styles.avatar} address={account.address}/>
+                {account.isScripted && <div className={styles.scripted_icon}/>}
+                <div className={styles.textContainer}>
+                    <div className={styles.name}>{account!.label}</div>
+                    <div className={styles.balanceContainer}>
+                        <div className={styles.wavesText}>WAVES</div>
+                        <div className={styles.wavesBalance}>
+                            {visibleBalance(account.wavesBalance)}
+                        </div>
+                    </div>
+                </div>
+            </div>}
+        <div className={classNames(styles.head_arrow, {[styles.head_arrow__open]: isOpened})}/>
+    </div>));
+
+const NoAccount = () => (
+    <div className={styles.content_empty}>
+        <div className={styles.login_icon}/>
+        <div className={styles.addText}>Add Account</div>
+    </div>
+);
+
+export default AccountHead;
+
+
