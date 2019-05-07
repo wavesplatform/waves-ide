e7e8bf4b95dd7d2a1b9b4ceaa8cb95ed6e15ebae
Sergey B <siemarell@users.noreply.github.com>
2019-05-07 14:20:10 +0300

    imporoved github samples sync to allow folders and tests. added test sample to library (#169)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,10 +2,12 @@ import * as m000 from './000';
 import * as m001 from './001';
 import * as m002 from './002';
 import * as m003 from './003';
+import * as m004 from './004';
 
 export default [
     m000,
     m001,
     m002,
-    m003
+    m003,
+    m004
 ];
