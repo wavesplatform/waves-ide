90a0210b8192046cfcbe23d03bb0597c06e2b203
Alexey Nagorny <anagorny@wavesplatform.com>
2019-06-10 11:35:21 +0300

    clear code

--- src/setupMonaco.ts
+++ src/setupMonaco.ts
@@ -2,8 +2,6 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { LspService } from '@waves/ride-language-server/LspService';
 import { Suggestions } from '@waves/ride-language-server/suggestions';
 import { MonacoLspServiceAdapter } from '@utils/MonacoLspServiceAdapter';
-import { languages } from "monaco-editor/esm/vs/editor/editor.api";
-import setLanguageConfiguration = languages.setLanguageConfiguration;
 
 const suggestions = new Suggestions();
 const transactionClasses = suggestions.types.find(({name}) => name === 'Transaction')!.type;
@@ -58,10 +56,6 @@ export default function setupMonaco() {
                     regex: new RegExp(`/\\b${suggestions.functions
                         .map(({name}) => ['*', '/', '+'].includes(name) ? `\\${name}` : name).join('|')}/\\b`)
                 },
-                // {
-                //     action: {token: 'typesItalic'},
-                //     regex: /\bAddress|Alias|Transfer|Order|DataEntry|GenesisTransaction|PaymentTransaction\b/
-                // },
                 {regex: /'/, action: {token: 'literal', bracket: '@open', next: '@base58literal'}},
                 {regex: /'/, action: {token: 'literal', bracket: '@open', next: '@base64literal'}},
                 {include: '@whitespace'},
@@ -78,9 +72,7 @@ export default function setupMonaco() {
 
             ],
             whitespace: [
-                //{ regex: /^[ \t\v\f]*#\w.*$/, action: { token: 'namespace.cpp' } },
                 {regex: /[ \t\v\f\r\n]+/, action: {token: 'white'}},
-                //{ regex: /\/\*/, action: { token: 'comment', next: '@comment' } },
                 {regex: /#.*$/, action: {token: 'comment'}},
             ],
             base58literal: [
@@ -105,7 +97,6 @@ export default function setupMonaco() {
         keywords, transactionClasses
     };
 
-    //monaco.languages.setLanguageConfiguration(LANGUAGE_ID, {})
     monaco.languages.setLanguageConfiguration(LANGUAGE_ID, {
         brackets: [['{', '}'], ['(', ')']],
         comments: { lineComment: '#' }
@@ -126,8 +117,6 @@ export default function setupMonaco() {
         provideSignatureHelp: languageService.signatureHelp.bind(languageService),
     });
 
-    // monaco.languages.setLanguageConfiguration()
-
     monaco.editor.defineTheme(DEFAULT_THEME_ID, {
         base: 'vs',
         colors: {},
@@ -136,13 +125,11 @@ export default function setupMonaco() {
             {token: 'keyword', foreground: '0000ff'},
             {token: 'string', foreground: 'a31415'},
             {token: 'globalFunctions', foreground: '484292', fontStyle: 'italic'},
-            //{token: 'number', foreground: '8e5c94'},
             {token: 'typesItalic', foreground: '4990ad', fontStyle: 'italic'},
             {token: 'types', foreground: '4990ad'},
             {token: 'literal', foreground: 'a31415', fontStyle: 'italic'},
             {token: 'directive', foreground: '#ff8b1e'},
             {token: 'annotation', foreground: 'f08c3a', fontStyle: 'bold'}
-            // {token: 'comment', foreground: '757575'}
         ]
     });
 
