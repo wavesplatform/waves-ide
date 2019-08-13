fb9d0cbe81d7e5601b94b856df2b0f505901443c
Alexey Nagorny <35231749+chlenc@users.noreply.github.com>
2019-08-13 12:37:57 +0300

    IDE-401/add complexity (#233)
    * :large_blue_diamond: add complexity
    
    * :large_blue_diamond: error compilation highlighting
    
    * less paddings

--- src/components/ReplsPanel/ReplTab.tsx
+++ src/components/ReplsPanel/ReplTab.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import cn from 'classnames';
 import styles from './styles.less';
 
 interface IProps {
     name: string
     label?: string
+    isError?: boolean
     onClick: () => void
 }
 
@@ -13,7 +14,7 @@ const ReplTab = (props: IProps) => {
         <div className={styles.replTab} onClick={props.onClick}>
             <div className={styles.replTab_name}>{props.name}</div>
             {props.label && (
-                <div className={styles.replTab_label}>
+                <div className={cn(styles.replTab_label, {[styles.replTab_label_error]: props.isError})}>
                     {props.label}
                 </div>
             )}
