d37ef256571a215e51e01538670227990b6ab6dd
siem <siemarell@gmail.com>
2019-09-26 11:04:39 +0300

    input

--- src/components/NewRepl/components/Input/index.tsx
+++ src/components/NewRepl/components/Input/index.tsx
@@ -1,19 +1,82 @@
 import * as React from 'react';
 import styles from './styles.less';
+import { DARK_THEME_ID, DEFAULT_THEME_ID, LANGUAGE_ID } from '@src/setupMonaco';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
+import MonacoEditor from 'react-monaco-editor';
 
 interface IProps {
 
 }
 
 interface IState {
-
+    // value: string
 }
 
-export class Input extends React.Component<IProps, IState> {
+@observer
+export class Input extends React.Component<IProps> {
+    @observable value: string = '';
+
+    @action
+    onChange = (value: string) => this.value = value;
+
+    onKeyPress = (e: monaco.IKeyboardEvent) => {
+        const code = e.browserEvent.code;
+        console.log(code);
+    };
+    editorDidMount = (e: monaco.editor.ICodeEditor, m: typeof monaco) => {
+        e.onKeyDown(e => this.onKeyPress(e));
+        e.focus();
+    };
 
     render() {
+
+        const options: monaco.editor.IEditorConstructionOptions = {
+            language: LANGUAGE_ID,
+            selectOnLineNumbers: false,
+            glyphMargin: false,
+            autoClosingBrackets: 'always',
+            minimap: {enabled: false},
+            contextmenu: false,
+            renderLineHighlight: 'none',
+            scrollBeyondLastLine: false,
+            scrollbar: {vertical: 'hidden', horizontal: 'hidden'},
+            overviewRulerLanes: 0,
+            wordBasedSuggestions: true,
+            acceptSuggestionOnEnter: 'on',
+            acceptSuggestionOnCommitCharacter: true,
+            find: {
+                seedSearchStringFromSelection: false,
+                autoFindInSelection: false,
+                addExtraSpaceOnTop: false,
+            },
+            matchBrackets: true,
+            lineNumbers: 'off' as 'off',
+            overviewRulerBorder: false,
+            lineDecorationsWidth: 0,
+            // ariaLabel: false,
+            codeLens: false,
+            // rulers: false,
+            colorDecorators: false,
+            extraEditorClassName: 'cli',
+            folding: false,
+            fontSize: 12,
+            lineHeight: 22,
+            fixedOverflowWidgets: true,
+            // quickSuggestionsDelay: 10,
+            // accessibilitySupport: 'off',
+        };
+
         return <div className={styles.root}>
-            <input/>
+            <MonacoEditor
+                value={this.value}
+                theme={DEFAULT_THEME_ID}
+                height={26}
+                options={options}
+                onChange={this.onChange}
+                editorDidMount={this.editorDidMount}
+            />
         </div>;
 
     }
--- src/components/ReplsPanel/RideRepl/index.tsx
+++ src/components/ReplsPanel/RideRepl/index.tsx
@@ -3,6 +3,7 @@ import { repl } from '@waves/ride-js';
 import { observable } from 'mobx';
 import { Line } from '@components/NewRepl/components/Line';
 import { Input } from '@components/NewRepl/components/Input';
+import styles from './styles.less';
 
 interface IProps {
 
@@ -12,30 +13,33 @@ interface IHistoryItemProps {
     command: string
     response: string[]
 }
+
 export default class RideRepl extends React.Component<IProps> {
 
     private repl = repl();
 
-    @observable history: IHistoryItemProps[] = [];
+    @observable history: IHistoryItemProps[] = [{command: 'abra', response:["cadabra"]}];
 
     componentDidMount(): void {
 
     }
 
-    render(){
-        return <div>
-            {this.history.map(item => <HistoryItem {...item}/>)}
+    render() {
+        return <div className={styles.root}>
+            <div className={styles.content}>
+                {this.history.map(item => <HistoryItem {...item}/>)}
+            </div>
             <Input></Input>
-        </div>
+        </div>;
     }
 }
 
 class HistoryItem extends React.Component<IHistoryItemProps> {
-    render(){
-        const {command, response} =  this.props;
+    render() {
+        const {command, response} = this.props;
         return <>
             <Line value={command}/>
             {response.map(item => <Line value={item}/>)}
-        </>
+        </>;
     }
 }
