8962a96ce43fa124b419552b0880121b4b4cde08
Alexey Nagorny <anagorny@wavesplatform.com>
2019-11-13 12:26:39 +0300

    refactor styles

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles.less';
 import { TNotifyOptions } from '@stores/NotificationsStore';
-import ScrollBar from "react-perfect-scrollbar";
+import ScrollBar from 'react-perfect-scrollbar';
 
 interface IProps {
     type: 'error' | 'info' | 'warning' | 'success',
