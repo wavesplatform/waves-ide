4d38936e5cc53befc8902fa865015f0d851b3f6b
Alexey Nagorny <anagorny@wavesplatform.com>
2019-12-16 13:47:15 +0300

    IDE-545/fix_mainPanelFooter_buttons

--- src/layout/Main/MainPanelFooter/ContractFooter/index.tsx
+++ src/layout/Main/MainPanelFooter/ContractFooter/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { inject, observer } from 'mobx-react';
-import { IRideFile, SettingsStore, SignerStore, NotificationsStore } from '@stores';
+import { IRideFile, NotificationsStore, SettingsStore, SignerStore } from '@stores';
 import classNames from 'classnames';
 import Button from '@src/components/Button';
 import copyToClipboard from 'copy-to-clipboard';
 import styles from '../styles.less';
 import ShareFileButton from '../ShareFileButton';
+import Dropdown from '@components/Dropdown';
+import ReactResizeDetector from 'react-resize-detector';
 
 interface IInjectedProps {
     settingsStore?: SettingsStore
@@ -19,9 +21,16 @@ interface IProps extends IInjectedProps, RouteComponentProps {
     file: IRideFile,
 }
 
+interface IState {
+    currentWidth: number
+}
+
 @inject('settingsStore', 'signerStore', 'notificationsStore')
 @observer
-class ContractFooter extends React.Component<IProps> {
+class ContractFooter extends React.Component<IProps, IState> {
+    state = {
+        currentWidth: 0
+    }
 
     handleDeploy = (tx: string) => () => {
         this.props.signerStore!.setTxJson(tx);
@@ -54,6 +63,18 @@ class ContractFooter extends React.Component<IProps> {
         deployHandler = txTemplate ? this.handleDeploy(txTemplate) : undefined;
         issueHandler = issueTemplate && file.info.type === 'asset' ? () => this.handleIssue(issueTemplate) : undefined;
 
+        const buttons: JSX.Element[] = [];
+
+        !file.readonly && buttons.push(<ShareFileButton key={1} file={file}/>);
+        file.info.type !== 'library' && buttons.push(<CopyBase64Button key={2} copyBase64Handler={copyBase64Handler}/>);
+        file.info.type === 'asset' && buttons.push(<IssueButton key={3} issueHandler={issueHandler}/>);
+        file.info.type !== 'library' && buttons.push(<DeployButton key={4} deployHandler={deployHandler}
+                                                                   type={file.info.type}/>);
+        //200
+
+        const count = Math.floor((this.state.currentWidth - 200) / 130);
+        const length = buttons.length - (count > buttons.length ? buttons.length : count)
+        const hiddenButtons = Array.from({length}, () => buttons.pop());
 
         return <div className={rootClassName}>
             <div className={styles.scriptInfo}>
@@ -65,23 +86,25 @@ class ContractFooter extends React.Component<IProps> {
                     <span className={styles!.boldText}> {file.info.complexity} / {file.info.maxComplexity}</span>
                 </span>
             </div>
-
+            <ReactResizeDetector handleWidth onResize={width => this.setState({currentWidth: width})}/>
             <div className={styles.buttonSet}>
-
-                {!file.readonly && <ShareFileButton file={file}/>}
-
-                {file.info.type !== 'library' && <CopyBase64Button copyBase64Handler={copyBase64Handler}/>}
-
-                {file.info.type === 'asset' && <IssueButton issueHandler={issueHandler}/>}
-
-                {file.info.type !== 'library' && <DeployButton deployHandler={deployHandler} type={file.info.type}/>}
-
+                {buttons}
+                {hiddenButtons.length > 0 && <Dropdown
+                    trigger={['click']}
+                    menuClassName={styles.dropdownBtn}
+                    overlay={<div className={styles.dropdown}>{hiddenButtons}</div>}
+                    button={<div className={styles['hidden-tabs-btn']}>
+                        <div className={styles.listIcn}/>
+                    </div>}
+                />}
             </div>
+
+
         </div>;
     }
 }
 
-const CopyBase64Button = ({copyBase64Handler}: { copyBase64Handler?: () => void }) =>
+const CopyBase64Button: React.FunctionComponent<{ copyBase64Handler?: () => void }> = ({copyBase64Handler}) =>
     <Button type="action-gray" disabled={!copyBase64Handler}
             onClick={copyBase64Handler}
             title="Copy base64 compiled script to clipboard"
@@ -90,7 +113,7 @@ const CopyBase64Button = ({copyBase64Handler}: { copyBase64Handler?: () => void 
         BASE64
     </Button>;
 
-const IssueButton = ({issueHandler}: { issueHandler?: () => void }) =>
+const IssueButton: React.FunctionComponent<{ issueHandler?: () => void }> = ({issueHandler}) =>
     <Button type="action-blue"
             disabled={!issueHandler}
             onClick={issueHandler}
@@ -98,7 +121,7 @@ const IssueButton = ({issueHandler}: { issueHandler?: () => void }) =>
         Issue
     </Button>;
 
-const DeployButton = ({deployHandler, type}: { deployHandler?: () => void, type: string }) =>
+const DeployButton: React.FunctionComponent<{ deployHandler?: () => void, type: string }> = ({deployHandler, type}) =>
     <Button type="action-blue"
             disabled={!deployHandler}
             onClick={deployHandler}
