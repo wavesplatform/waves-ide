909b80cf728e03aeee0d4743da8a78cfdfc56387
Алешенька <alexey@Alesenkas-MacBook-Pro.local>
2020-05-13 19:23:07 +0300

    :tada:

---
+++ src/layout/Dialogs/MigrationDialog/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.less';
+
+export default class MigrationDialog extends React.Component {
+
+    render() {
+        return <div className={styles.root}>
+            <div className={styles.body}>
+                Hello world
+            </div>
+        </div>;
+    }
+
+}
