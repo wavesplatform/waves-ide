aa45033eebfe74035f0625a63e248c162cec221e
Alexey Nagorny <anagorny@wavesplatform.com>
2020-06-01 17:58:19 +0300

    add migrationStore

--- src/layout/Dialogs/MigrationDialog/index.tsx
+++ src/layout/Dialogs/MigrationDialog/index.tsx
@@ -3,97 +3,28 @@ import Dialog from '@src/components/Dialog';
 import Button from '@src/components/Button';
 import styles from './styles.less';
 import { inject, observer } from 'mobx-react';
-import { AccountsStore, SettingsStore } from '@stores';
-import NotificationsStore from '@stores/NotificationsStore';
-import { Bus, WindowAdapter } from '@waves/waves-browser-bus';
+import { MigrationStore } from '@stores';
 import { Loading } from '@src/layout/Dialogs/MigrationDialog/Loading';
 
 interface IProps {
-    accountsStore?: AccountsStore
-    notificationsStore?: NotificationsStore
-    settingsStore?: SettingsStore
+    migrationStore?: MigrationStore
 }
 
-interface IState {
-    ready: boolean
-    success: boolean
-}
-
-const url = 'http://localhost:8080';
-// const url = 'https://waves-ide.com';
-
-
-@inject('accountsStore', 'settingsStore', 'notificationsStore')
+@inject('migrationStore')
 @observer
-export default class MigrationDialog extends React.Component<IProps, IState> {
-
-    private bus: Bus | null = null;
-
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {ready: false, success: false};
-        const isSafari = false;
-        // const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
-        const init = (adapter: any) => {
-            const bus = new Bus(adapter);
-            // bus.once('ready', () => {
-            //     this.setState({ready: true});
-            // });
-            bus.on('migration-success', () => {
-                this.setState({success: true});
-            });
-        };
-
-        if (isSafari) {
-            const win = window.open(url);
-            WindowAdapter.createSimpleWindowAdapter(win as any, {origins: '*'}).then(init);
-        } else {
-
-            const iframe = document.createElement('iframe');
-            WindowAdapter.createSimpleWindowAdapter(iframe, {origins: '*'}).then(init);
-
-            iframe.src = url;
-            iframe.className = styles.iframe;
-            document.body.appendChild(iframe);
-            this.initIframeStatusWatcher();
-        }
-    }
-
-    initIframeStatusWatcher(): void {
-        let counter = 0;
-        const interval = setInterval(async () => {
-            counter++;
-            this.bus && this.bus.request('ready').then(() => {
-                this.setState({ready: true});
-                clearInterval(interval);
-            });
-        }, 1000);
-
-        if (counter >= 10) {
-            clearInterval(interval);
-        }
-
-    }
-
-    handleMigrate = () => {
-        this.setState({ready: false});
-        this.bus && this.bus.dispatchEvent('migrate', this.props.settingsStore!.JSONState);
-    };
-
-    handleOpenIde = () => window.open(url);
+export default class MigrationDialog extends React.Component<IProps> {
 
     render() {
-        const {ready, success} = this.state;
+        const {ready, success, dispatchMigration, handleOpenIde} = this.props.migrationStore!;
         return <Dialog
             className={styles.dialog}
             title="Move IDE"
             width={618}
             footer={<>{success
-                ? <Button className={styles.btn} type="action-blue" onClick={this.handleOpenIde} disabled={!success}>
+                ? <Button className={styles.btn} type="action-blue" onClick={handleOpenIde} disabled={!success}>
                     Open new IDE
                 </Button>
-                : <Button className={styles.btn} type="action-blue" onClick={this.handleMigrate} disabled={!ready}>
+                : <Button className={styles.btn} type="action-blue" onClick={dispatchMigration} disabled={!ready}>
                     {ready ? 'Migrate' : <Loading/>}
                 </Button>}
             </>
