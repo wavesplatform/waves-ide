8cef867c2a232c429eec6c51576de0c452876729
Алешенька <alexey@Alesenkas-MacBook-Pro.local>
2020-06-03 15:46:03 +0300

    add func complexiti in failed funcs

--- src/stores/CompilationStore.ts
+++ src/stores/CompilationStore.ts
@@ -12,14 +12,20 @@ export default class CompilationStore extends SubStore {
     @computed
     get compilation() {
         const file = this.rootStore.filesStore.currentFile;
-
         let compilation: { type: 'error' | 'success', message: string }[] = [];
+        const getFuncComplexityOut = (complexityByFunc?: object, type: 'error' | 'success' = 'success') =>
+            Object.entries(complexityByFunc || {})
+                .map(([func, complexity]) => ({type, message: `${func} complexity ${complexity}`}));
 
         if (file && file.type !== FILE_TYPE.MARKDOWN && file.info) {
             const {name, info} = file;
             if ('error' in info.compilation) {
                 compilation.length = 0;
                 compilation.push({type: 'error', message: info.compilation.error});
+                compilation = [
+                    ...compilation,
+                    ...getFuncComplexityOut((info.compilation as any).complexityByFunc, 'error')
+                ];
 
             } else {
                 compilation.length = 0;
@@ -28,15 +34,10 @@ export default class CompilationStore extends SubStore {
                     type: 'success',
                     message: `Script complexity ${info.compilation.result.complexity}`
                 });
-                if ('complexityByFunc' in info.compilation.result && info.compilation.result.complexityByFunc) {
-                    compilation = [...compilation, ...Object.entries(info.compilation.result.complexityByFunc)
-                        .map(([func, complexity]) => ({
-                            type: 'success' as 'success',
-                            message: `${func} complexity ${complexity}`
-                        }))
-                    ];
-                }
-
+                compilation = [
+                    ...compilation,
+                    ...getFuncComplexityOut((info.compilation.result as any).complexityByFunc)
+                ];
             }
         }
         return compilation;
