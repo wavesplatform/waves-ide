4a2f66f44e044767bfe6547ba19cc459841b9e02
inazarov <inazarov@web3tech.ru>
2021-11-16 14:21:07 +0300

    rename browsers > overrideBrowserslist

--- scripts/build-monaco.js
+++ scripts/build-monaco.js
@@ -58,7 +58,7 @@ webpack({
                                 require('postcss-flexbugs-fixes'),
                                 require('postcss-inline-svg'),
                                 autoprefixer({
-                                    browsers: [
+                                    overrideBrowserslist: [
                                         '>1%',
                                         'last 4 versions',
                                         'Firefox ESR',
